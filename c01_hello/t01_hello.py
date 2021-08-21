d7c0164034a3c508f60b276cdcad8a8fb7e35574
Ray <ray1024_b@163.com>
2021-08-21 14:04:40 +0800

    commit t01_hello.py

--- c01_hello/t01_hello.py
+++ c01_hello/t01_hello.py
@@ -2,12 +2,17 @@
 
 def hello():
     """
-    commit 后，修改文件，显示为蓝色
+    commit 后，修改文件，project view 中文件显示为蓝色
+
+    这一行可以删除
 
     :return:
     """
     print("hello, this is a test")
     print("wow, I forgot python")
+
+    print("ok, you can delete")
+
 
 
 hello()
